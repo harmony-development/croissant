fe4960f02f49a57d0fa5e0261dec0a0f03f8f1ae
Alexey Peschany <nelex111@gmail.com>
2021-01-05 14:37:36 +0300

    Drawer: add guild list item

--- lib/routes/main/drawer.dart
+++ lib/routes/main/drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:harmony_sdk/harmony_sdk.dart';
 import 'package:provider/provider.dart';
 import 'package:winged_staccato/routes/main/circle_button.dart';
+import 'package:winged_staccato/routes/main/guild_item.dart';
 import 'package:winged_staccato/routes/main/invite_dialog.dart';
 
 import '../utils.dart';
@@ -48,40 +49,7 @@ class ChonkyDrawer extends StatelessWidget {
           itemCount: state.guilds == null ? 0 : state.guilds.length,
           itemBuilder: (BuildContext context, int index) {
             Guild g = state.guilds[index];
-            return FutureBuilder(
-              future: g.picture,
-              builder: (context, snapshot) {
-                if (snapshot.hasData && !snapshot.hasError) {
-                  String link = snapshot.data;
-                  String picture;
-                  if (link.startsWith(Utils.hmcScheme))
-                    picture = Utils.fixHarmonyContentUrl(link);
-                  else
-                    picture = link;
-                  return Ink(
-                    height: 64,
-                    padding: EdgeInsets.all(8),
-                    color: g.id == state.selectedChannel?.guildId ? Colors.pinkAccent : Colors.transparent,
-                    child: CircleButton(
-                      child: CircleAvatar(
-                        backgroundColor: Colors.grey,
-                        backgroundImage: NetworkImage(picture),
-                      ),
-                      onClick: () async {
-                        if (g.id != state.selectedChannel?.guildId) {
-                          await Provider.of<MainState>(context, listen: false).updateChannels(index);
-                        }
-                      },
-                    ));
-                } else {
-                  return Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-                }
-              });
+            return GuildItem(index);
           }),
         Container(
           height: 64,
@@ -110,22 +78,7 @@ class ChonkyDrawer extends StatelessWidget {
           height: 100,
           margin: EdgeInsets.only(top: statusBarHeight),
           child: Center(
-            child: state.selectedChannel == null ? Container() :
-            FutureBuilder(
-              future: state.selectedGuild.name,
-              builder: (context, snapshot) {
-                if (snapshot.hasData && !snapshot.hasError) {
-                  return Text(snapshot.data);
-                } else {
-                  return Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-                }
-              }
-            ),
+            child: state.selectedChannel == null ? Container() : Text(state.selectedGuild.name),
           ),
           decoration: BoxDecoration(
             color: Colors.blue,
---
+++ lib/routes/main/guild_item.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:harmony_sdk/harmony_sdk.dart';
+import 'package:provider/provider.dart';
+
+import '../utils.dart';
+import 'circle_button.dart';
+import 'state.dart';
+
+class GuildItem extends StatefulWidget {
+  final int index;
+
+  GuildItem(this.index);
+
+  @override
+  _GuildItemState createState() => _GuildItemState();
+}
+
+class _GuildItemState extends State<GuildItem> {
+  int _refreshCount;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshCount = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    MainState state = Provider.of<MainState>(context);
+    Guild guild = state.guilds[widget.index];
+    String avatar = guild.avatar;
+
+    if (avatar == null) {
+      loadGuildInfo(state);
+      return Ink(
+        height: 64,
+        child: CircleButton(
+          child: CircleAvatar(backgroundColor: Colors.grey,),
+        )
+      );
+    }
+
+    Widget avatarWidget;
+    if (avatar.isEmpty)
+      avatarWidget = CircleAvatar(
+        backgroundColor: Colors.grey,
+      );
+    else {
+      String avatarUrl = avatar.startsWith(Utils.hmcScheme)
+          ? Utils.fixHarmonyContentUrl(avatar)
+          : avatar;
+      avatarWidget = CircleAvatar(
+        backgroundColor: Colors.grey,
+        backgroundImage: NetworkImage(avatarUrl),
+      );
+    }
+
+    return Ink(
+      height: 64,
+      color: _isSelected(state) ? Colors.pinkAccent : Colors.transparent,
+      child: CircleButton(
+        child: avatarWidget,
+        onClick: () async {
+          if (!_isSelected(state)) {
+            await Provider.of<MainState>(context, listen: false)
+                .updateChannels(widget.index);
+          }
+        },
+      ));
+  }
+
+  bool _isSelected(MainState state) => state.guilds[widget.index].id == state.selectedChannel?.guild;
+
+  Future<void> loadGuildInfo(MainState state) async {
+    await state.guilds[widget.index].refresh();
+    setState(() {
+      _refreshCount += 1;
+    });
+  }
+}
