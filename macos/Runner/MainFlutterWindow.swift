9ffd735e46b67df8aa90a4a8b9cf6c73bed3ae92
spiral <spiral@spiral.sh>
2022-02-27 15:02:25 -0500

    feat: add macOS build

---
+++ macos/Runner/MainFlutterWindow.swift
@@ -0,0 +1,16 @@
+import Cocoa
+import FlutterMacOS
+
+class MainFlutterWindow: NSWindow {
+  override func awakeFromNib() {
+    let flutterViewController = FlutterViewController.init()
+    self.setFrame(NSRect(x:0,y:0,width: 1280,height: 720), display: true)
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
+    self.setFrame(windowFrame, display: true)
+
+    RegisterGeneratedPlugins(registry: flutterViewController)
+
+    super.awakeFromNib()
+  }
+}
